5278d2fbdc6de244de63c0ea8fda58f0f57dab5b
Adriana Rios <amrios2817@gmail.com>
2021-02-07 16:26:51 -0500

    Add Fun component

---
+++ components/content/fun.js
@@ -0,0 +1,21 @@
+import Split from "../blocks/split";
+import { jobs } from "../../utils/copy";
+import BodyText from "../blocks/body-text";
+import TitleText from "../blocks/title-text";
+
+const { fun } = jobs;
+const RightHeadline = () => <TitleText>{fun.heading}</TitleText>;
+
+const Fun = () => (
+  <Split>
+    <Split.Left>
+      <img src="../../../static/images/fun.png" alt={fun.alt} />
+    </Split.Left>
+    <Split.Right>
+      <RightHeadline>{fun.heading}</RightHeadline>
+      <BodyText>{fun.body}</BodyText>
+    </Split.Right>
+  </Split>
+);
+
+export default Fun;
--- pages/jobs.js
+++ pages/jobs.js
@@ -4,7 +4,7 @@ import Join from "../components/content/join";
 import IconInfo from "../components/content/icon-info";
 import PushingOurselves from "../components/content/pushing-ourselves";
 import Diversity from "../components/content/diversity";
-// import Fun from "../components/content/fun";
+import Fun from "../components/content/fun";
 // import Perks from "../components/content/perks";
 // import { PerksProvider } from "../context/PerksContext";
 // import Openings from "../components/content/openings";
@@ -20,6 +20,7 @@ function Jobs() {
       <IconInfo />
       <PushingOurselves />
       <Diversity />
+      <Fun />
     </div>
   );
 }
