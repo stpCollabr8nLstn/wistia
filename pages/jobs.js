3bba07b85e41f8b99eab303eb0d3e0cfdb661bea
Adriana Rios <amrios2817@gmail.com>
2021-02-07 15:58:08 -0500

    Make copy updates

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,5 +1,25 @@
+import React from "react";
+import BeYourself from "../components/content/be-yourself";
+import Join from "../components/content/join";
+import IconInfo from "../components/content/icon-info";
+// import PushingOurselves from "../components/content/pushing-ourselves";
+// import Diversity from "../components/content/diversity";
+// import Fun from "../components/content/fun";
+// import Perks from "../components/content/perks";
+// import { PerksProvider } from "../context/PerksContext";
+// import Openings from "../components/content/openings";
+
 function Jobs() {
-  return <div>Jobs page!</div>;
+  React.useEffect(() => {
+    document.title = "Wistia Jobs!";
+  });
+  return (
+    <div>
+      <Join />
+      <BeYourself />
+      <IconInfo />
+    </div>
+  );
 }
 
 export default Jobs;
